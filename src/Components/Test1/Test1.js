a0f4f0fd0fcc7a376fc21944a8d85b46a72721bc
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2022-07-01 21:43:19 +0600

    redux added

--- src/Components/Test1/Test1.js
+++ src/Components/Test1/Test1.js
@@ -1,11 +1,45 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  saveData,
+} from "../../Redux/Slices/counterSlice";
 const Test1 = () => {
-    return (
-        <div className=' h-screen'>
-            <h1>Test 1 </h1>
-        </div>
-    );
+  const count = useSelector((state) => state.value);
+  const user = useSelector((state) => state.user);
+  console.log(user);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const loadData = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => dispatch(saveData(data)));
+    };
+
+    loadData();
+  }, []);
+  return (
+    <div className=" h-screen m-3 p-2">
+      <h1>Test 1 </h1>
+      <button
+        onClick={() => dispatch(increment())}
+        className=" py-2 px-4 text-2xl"
+      >
+        +
+      </button>
+      <h1 className=" py-2 px-4 text-2xl">{count}</h1>
+      <button
+        onClick={() => dispatch(decrement())}
+        className=" py-2 px-4 text-2xl"
+      >
+        -
+      </button>
+
+      <div>{user && user?.map((item) => <h1>{item?.name}</h1>)}</div>
+    </div>
+  );
 };
 
 export default Test1;
