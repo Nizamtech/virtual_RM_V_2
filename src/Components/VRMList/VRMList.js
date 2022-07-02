160a50f33c628552a6a7241161df9938ef0bfba4
Rukaiya Amin <rokeya@aamartaka.com>
2022-07-02 10:26:06 +0600

    vrm list api added

--- src/Components/VRMList/VRMList.js
+++ src/Components/VRMList/VRMList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import VrmListTable from "./VrmListTable";
+import axios from "axios";
 
 const VRMList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:8000/accounts/agent/register/`).then((res) => {
+      setUsers(res.data.results);
+    });
+  }, []);
+  console.log(users);
   return (
     <div className=" h-screen p-3 m2">
-      <VrmListTable />
+      <VrmListTable users={users}/>
     </div>
   );
 };
