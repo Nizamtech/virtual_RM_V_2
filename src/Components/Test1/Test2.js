04759f332e9e063126d3be5eb953929d3d4cc834
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2022-07-12 23:02:32 +0600

    tinymce added

--- src/Components/Test1/Test2.js
+++ src/Components/Test1/Test2.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import TextEditor from "../Feature/TextEditor/TextEditor";
 
 const Test2 = () => {
-    return (
-        <div  className=' h-screen'>
-            <h1>Test2  </h1>
-        </div>
-    );
+  return (
+    <div className=" h-screen">
+      <TextEditor />
+    </div>
+  );
 };
 
 export default Test2;
