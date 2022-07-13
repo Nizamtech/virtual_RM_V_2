5441ad0107c05638323b3c369425ffb9177120c7
nizam <nizamtech@aamartaka.com>
2022-07-13 17:32:45 +0600

    sidebar icon is done

--- src/Components/Feature/FeatureTable.js
+++ src/Components/Feature/FeatureTable.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { deleteAlert } from "../../Shared/Alert/deleteAlert";
 import { Link } from "react-router-dom";
 import DOMPurify from "dompurify";
 
--- src/Shared/Sidebar/Sidebar.js
+++ src/Shared/Sidebar/Sidebar.js
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import Collapsible from "react-collapsible";
+
 import {
   faUsers,
   faUserPlus,
@@ -9,7 +11,18 @@ import {
   faCreditCard,
   faHandHoldingDollar,
 } from "@fortawesome/free-solid-svg-icons";
-
+import { BsChevronDown } from "react-icons/bs";
+import { GiReceiveMoney } from "react-icons/gi";
+import { TbLayoutDashboard } from "react-icons/tb";
+import { FaUsers } from "react-icons/fa";
+import { SiGoogleads } from "react-icons/si";
+import { FaMoneyCheck } from "react-icons/fa";
+import { MdOutlineFeaturedPlayList } from "react-icons/md";
+import { BsFileCheckFill } from "react-icons/bs";
+import { MdForwardToInbox } from "react-icons/md";
+import { TbActivityHeartbeat } from "react-icons/tb";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BsListCheck } from "react-icons/bs";
 const Sidebar = () => {
   const loccation = useLocation();
   return (
@@ -26,42 +39,23 @@ const Sidebar = () => {
         <ul className="space-y-2  ">
           <li>
             <NavLink
-              to="/test1"
+              to="/test2"
               className={`${
-                loccation.pathname.includes("test1") && "activeLink"
+                loccation.pathname.includes("test2") && "activeLink"
               } use"h-10  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-              </svg>
+              <TbLayoutDashboard className=" text-white" size={30} />
               <span className="ml-2 text-white">Dashboard </span>
             </NavLink>
           </li>
-          <li className=" text-white flex   ml-2">
-            <svg
-              className=" flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <details className=" text-white mx-2 mt-2 w-full ">
-              <summary className=" mb-1 flex justify-between items-center">
-                <h1>User Section</h1> <h1>v</h1>
-              </summary>
 
-              <div className=" bg-[#1B399D] mx-2 rounded-lg w-full">
+          <div className="ml-2 flex w-full justify-center">
+            <FaUsers className=" text-white" size={30} />
+            <Collapsible
+              className="mx-3 px-1 w-full text-base font-normal "
+              trigger={["User Section", <BsChevronDown />]}
+            >
+              <li className="  bg-[#1B399D] text-white w-full my-2 pb-2">
                 <NavLink
                   to="/createteam"
                   className={`${
@@ -103,27 +97,17 @@ const Sidebar = () => {
                   {/* <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg> */}
                   <span className="ml-2">Manage User</span>
                 </NavLink>
-              </div>
-            </details>
-          </li>
-          <li className=" text-white flex  ml-2 ">
-            <svg
-              className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              </li>
+            </Collapsible>
+          </div>
+
+          <div className="ml-2 flex w-full justify-center ">
+            <GiReceiveMoney className=" text-white" size={30} />
+            <Collapsible
+              className="mx-3 px-1 w-full text-base font-normal "
+              trigger={["Benefits", <BsChevronDown />]}
             >
-              <path
-                fill-rule="evenodd"
-                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <details className=" text-white  mx-2 mt-1 w-full">
-              <summary className=" mb-1 flex justify-between items-center">
-                <h1>Benefits</h1> <h1>v</h1>
-              </summary>
-              <div className=" bg-[#1B399D] rounded-lg w-full  ">
+              <li className="  bg-[#1B399D] text-white w-full my-2 pb-2">
                 <NavLink
                   to="/cardcommission"
                   className={`${
@@ -166,9 +150,9 @@ const Sidebar = () => {
 
                   <span className="ml-2 text-xs">Loan Commission List</span>
                 </NavLink>
-              </div>
-            </details>
-          </li>
+              </li>
+            </Collapsible>
+          </div>
           <li>
             <NavLink
               to="/vrmlist"
@@ -176,16 +160,8 @@ const Sidebar = () => {
                 loccation.pathname.includes("vrmlist") && "activeLink"
               } use"  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-              </svg>
-              <span className="ml-3 text-white active:text-black ">
+              <BsListCheck className=" text-white" size={30} />
+              <span className="ml-3 text-white  text-base font-normal active:text-black ">
                 VRM List
               </span>
             </NavLink>
@@ -197,31 +173,16 @@ const Sidebar = () => {
                 loccation.pathname.includes("leadlist") && "activeLink"
               } use"  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <SiGoogleads className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 Lead List
               </span>
             </NavLink>
           </li>
           <li className=" ml-2">
             <NavLink to="/" className=" flex">
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
-                <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <BsFileCheckFill className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 Submitted Lead
               </span>
             </NavLink>
@@ -234,19 +195,8 @@ const Sidebar = () => {
                 loccation.pathname.includes("paymentstatus") && "activeLink"
               } use"  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <FaMoneyCheck className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 Payment Status
               </span>
             </NavLink>
@@ -258,19 +208,8 @@ const Sidebar = () => {
                 loccation.pathname.includes("feature") && "activeLink"
               } use"  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <MdOutlineFeaturedPlayList className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 Feature
               </span>
             </NavLink>
@@ -282,41 +221,19 @@ const Sidebar = () => {
                 loccation.pathname.includes("inbox") && "activeLink"
               } use"  flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"`}
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <MdForwardToInbox className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 Inbox
               </span>
             </NavLink>
           </li>
           <li>
             <a
-              href="#"
+              href="/"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-white">
+              <TbActivityHeartbeat className=" text-white" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-white">
                 VRM Activity
               </span>
             </a>
@@ -326,19 +243,8 @@ const Sidebar = () => {
               href="#"
               className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-[#294AB3]  dark:hover:bg-gray-700"
             >
-              <svg
-                className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="flex-1 ml-3 whitespace-nowrap text-red-400">
+              <AiOutlineLogout className=" text-red-500" size={30} />
+              <span className="flex-1 ml-3 text-base font-normal text-red-400">
                 Log Out
               </span>
             </a>
