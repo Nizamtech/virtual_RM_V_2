0cb35ae28fc59cfc21324b76c65741d5bc9707f0
Nizam Uddin Babu <43912849+nizamseu@users.noreply.github.com>
2022-07-21 02:07:40 +0600

    from home add more

--- src/Components/Test1/Test2.js
+++ src/Components/Test1/Test2.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AddMore from "../AddMore/AddMore";
 import EditCardCommissionList from "../CardCommissionList/EditCardCommissionList";
+import TestForm from "../Test2/TestForm";
 
 const Test2 = () => {
   return (
     <div className=" h-screen">
       {/* <EditCardCommissionList /> */}
-      <AddMore />
+      {/* <AddMore /> */}
+      <TestForm />
     </div>
   );
 };
